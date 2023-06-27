faa439a0f9f0f067223fdfa5cb15515d87c4c58c
keebot-png <kee.bot@icloud.com>
2023-06-27 14:43:16 +0200

    Improve perceived performance of song page.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lp4iv2kn73"
+    "revision": "0.53fb392hrjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
